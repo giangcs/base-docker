7e3da46f937b94ead14bfb08c6a6bf9d99fa27d4
sgiang1610 <songiang1610@gmail.com>
2025-03-13 16:40:53 +0700

    working base

--- base-express-auth/src/app.js
+++ base-express-auth/src/app.js
@@ -1,49 +1,46 @@
-// import express from 'express';
-// import bodyParser from 'body-parser';
-// import logger from 'morgan';
-// import mongoose from 'mongoose';
-//
-// // Set up express app
-// const app = express();
-//
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// // const cors = require("cors");
-// // app.use(cors());
-//
-//
-// app.use(logger('dev'));
-// const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/x-auth";
-//
-// // Set up mongoose
-// const connectWithRetry = () => {
-//     console.log("Attempting to connect to MongoDB...");
-//     // { useNewUrlParser: true, useUnifiedTopology: true } no longer nedded
-//     mongoose.connect(MONGO_URI)
-//         .then(() => {
-//             console.log('Database connected');
-//         })
-//         .catch((error) => {
-//             console.log('Error connecting to database:', error);
-//             setTimeout(connectWithRetry, 5000);
-//         });
-// };
-// connectWithRetry();
-// // Set up port number
-// const port = process.env.PORT || 5000;
-//
-// // Set up home route
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: 'Welcome to Project Support',
-//     });
-// });
-//
-// app.listen(port, (request, respond) => {
-//     console.log(`Our server is live on ${port}. Yay!`);
-// });
-//
-// import apiRouter from './routes/index.js';
-// app.use('/api/', apiRouter);
-//
-// module.exports = app;
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import mongoose from 'mongoose';
+import expressListEndpoints from 'express-list-endpoints';
+
+// Set up express app
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/x-auth";
+
+// Set up mongoose
+const connectWithRetry = () => {
+    console.log("Attempting to connect to MongoDB...");
+    // { useNewUrlParser: true, useUnifiedTopology: true } no longer nedded
+    mongoose.connect(MONGO_URI)
+        .then(() => {
+            console.log('Database connected');
+        })
+        .catch((error) => {
+            console.log('Error connecting to database:', error);
+            setTimeout(connectWithRetry, 5000);
+        });
+};
+connectWithRetry();
+// Set up port number
+const port = process.env.PORT || 5000;
+
+// Set up home route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to Project Support',
+    });
+});
+
+app.listen(port, (request, respond) => {
+    console.log(`Our server is live on ${port}. Yay!`);
+});
+
+import apiRouter from './routes/index.js';
+app.use('/api/', apiRouter);
+
+// console.log(expressListEndpoints(app));
